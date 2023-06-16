f037a26e8f7f1c3dd6ba2f7ffea5741d404a756e
Emirkanmaz <emirkan.kanmaz.2001@gmail.com>
2023-06-16 15:20:09 +0300

    commit

---
+++ assets/js/mapdata.js
@@ -0,0 +1,734 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "1400", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: "1.5",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TUR2229: {
+      url: "./sehir/aydin",
+      color: "#295372",
+      description: "Kira: 9,814 ₺ <br/> m²: 78₺",
+      name: "Aydın",
+      zoomable: "no"
+    },
+    TUR2230: {
+      url: "./sehir/izmir",
+      color: "#295372",
+      description: "Kira: 11,971 ₺ <br/> m²: 101₺",
+      name: "İzmir",
+      zoomable: "no"
+    },
+    TUR2238: {
+      url: "./sehir/balikesir",
+      color: "#295372",
+      description: "Kira: 7,720 ₺ <br/> m²: 73₺",
+      name: "Balıkesir",
+      zoomable: "no"
+    },
+    TUR2239: {
+      url: "./sehir/canakkale",
+      color: "#295372",
+      description: "Kira: 7,132 ₺ <br/> m²: 73₺",
+      name: "Çanakkale",
+      zoomable: "no"
+    },
+    TUR2240: {
+      url: "./sehir/edirne",
+      color: "#376e97",
+      description: "Kira: 6,411 ₺ <br/> m²: 73₺",
+      name: "Edirne",
+      zoomable: "no"
+    },
+    TUR2241: {
+      url: "./sehir/kirklareli",
+      color: "#376e97",
+      description: "Kira: 5,336 ₺ <br/> m²: 55₺",
+      name: "Kirklareli",
+      zoomable: "no"
+    },
+    TUR2242: {
+      url: "./sehir/tekirdag",
+      color: "#295372",
+      description: "Kira: 7,217 ₺ <br/> m²: 63₺",
+      name: "Tekirdağ",
+      zoomable: "no"
+    },
+    TUR2263: {
+      url: "./sehir/bilecik",
+      color: "#6aa1ca",
+      description: "Kira: 4,236 ₺ <br/> m²: 49₺",
+      name: "Bilecik",
+      zoomable: "no"
+    },
+    TUR2264: {
+      url: "./sehir/bursa",
+      color: "#295372",
+      description: "Kira: 8,405 ₺ <br/> m²: 72₺",
+      name: "Bursa",
+      zoomable: "no"
+    },
+    TUR2265: {
+      url: "./sehir/istanbul",
+      color: "#295372",
+      description: "Kira: 15,014 ₺ <br/> m²: 130₺",
+      name: "İstanbul",
+      zoomable: "no"
+    },
+    TUR2266: {
+      url: "./sehir/kocaeli",
+      color: "#295372",
+      description: "Kira: 8,761 ₺ <br/> m²: 68₺",
+      name: "Kocaeli",
+      zoomable: "no"
+    },
+    TUR2267: {
+      url: "./sehir/sakarya",
+      color: "#295372",
+      description: "Kira: 7,352 ₺ <br/> m²: 66₺",
+      name: "Sakarya",
+      zoomable: "no"
+    },
+    TUR2268: {
+      url: "./sehir/bolu",
+      color: "#4589bc",
+      description: "Kira: 5,012 ₺ <br/> m²: 49₺",
+      name: "Bolu",
+      zoomable: "no"
+    },
+    TUR2269: {
+      url: "./sehir/eskisehir",
+      color: "#376e97",
+      description: "Kira: 5,592 ₺ <br/> m²: 64₺",
+      name: "Eskişehir",
+      zoomable: "no"
+    },
+    TUR2270: {
+      url: "./sehir/kastamonu",
+      color: "#6aa1ca",
+      description: "Kira: 3,924 ₺ <br/> m²: 41₺",
+      name: "Kastamonu",
+      zoomable: "no"
+    },
+    TUR2271: {
+      url: "./sehir/antalya",
+      color: "#295372",
+      description: "Kira: 13,747 ₺ <br/> m²: 132₺",
+      name: "Antalya",
+      zoomable: "no"
+    },
+    TUR2272: {
+      url: "./sehir/afyonkarahisar",
+      color: "#6aa1ca",
+      description: "Kira: 4,000 ₺ <br/> m²: 43₺",
+      name: "Afyonkarahisar",
+      zoomable: "no"
+    },
+    TUR2273: {
+      url: "./sehir/burdur",
+      color: "#90b9d7",
+      description: "Kira: 3,774 ₺ <br/> m²: 49₺",
+      name: "Burdur",
+      zoomable: "no"
+    },
+    TUR2274: {
+      url: "./sehir/denizli",
+      color: "#376e97",
+      description: "Kira: 5,474 ₺ <br/> m²: 61₺",
+      name: "Denizli",
+      zoomable: "no"
+    },
+    TUR2275: {
+      url: "./sehir/isparta",
+      color: "#4589bc",
+      description: "Kira: 4,399 ₺ <br/> m²: 51₺",
+      name: "Isparta",
+      zoomable: "no"
+    },
+    TUR2276: {
+      url: "./sehir/kutahya",
+      color: "#90b9d7",
+      description: "Kira: 3,662 ₺ <br/> m²: 48₺",
+      name: "Kütahya",
+      zoomable: "no"
+    },
+    TUR2277: {
+      url: "./sehir/manisa",
+      color: "#376e97",
+      description: "Kira: 6,336 ₺ <br/> m²: 61₺",
+      name: "Manisa",
+      zoomable: "no"
+    },
+    TUR2278: {
+      url: "./sehir/mugla",
+      color: "#295372",
+      description: "Kira: 27,516 ₺ <br/> m²: 219₺",
+      name: "Muğla",
+      zoomable: "no"
+    },
+    TUR2281: {
+      url: "./sehir/adiyaman",
+      color: "#376e97",
+      description: "Kira: 5,722 ₺ <br/> m²: 51₺",
+      name: "Adıyaman",
+      zoomable: "no"
+    },
+    TUR2282: {
+      url: "./sehir/elazig",
+      color: "#6aa1ca",
+      description: "Kira: 4,350 ₺ <br/> m²: 28₺",
+      name: "Elazığ",
+      zoomable: "no"
+    },
+    TUR2283: {
+      url: "./sehir/kahramanmaras",
+      color: "#376e97",
+      description: "Kira: 5,861 ₺ <br/> m²: 44₺",
+      name: "Kahramanmaraş",
+      zoomable: "no"
+    },
+    TUR2284: {
+      url: "./sehir/malatya",
+      color: "#376e97",
+      description: "Kira: 6,571 ₺ <br/> m²: 44₺",
+      name: "Malatya",
+      zoomable: "no"
+    },
+    TUR2285: {
+      url: "./sehir/mersin",
+      color: "#295372",
+      description: "Kira: 12,150 ₺ <br/> m²: 99₺",
+      name: "Mersin",
+      zoomable: "no"
+    },
+    TUR2286: {
+      url: "./sehir/kirsehir",
+      color: "#6aa1ca",
+      description: "Kira: 4,184 ₺ <br/> m²: 35₺",
+      name: "Kırşehir",
+      zoomable: "no"
+    },
+    TUR2287: {
+      url: "./sehir/kayseri",
+      color: "#4589bc",
+      description: "Kira: 5,103 ₺ <br/> m²: 35₺",
+      name: "Kayseri",
+      zoomable: "no"
+    },
+    TUR2288: {
+      url: "./sehir/nevsehir",
+      color: "#376e97",
+      description: "Kira: 6,094 ₺ <br/> m²: 64₺",
+      name: "Nevşehir",
+      zoomable: "no"
+    },
+    TUR2289: {
+      url: "./sehir/hatay",
+      color: "#295372",
+      description: "Kira: 8,493 ₺ <br/> m²: 63₺",
+      name: "Hatay",
+      zoomable: "no"
+    },
+    TUR2290: {
+      url: "./sehir/amasya",
+      color: "#4589bc",
+      description: "Kira: 4,353 ₺ <br/> m²: 45₺",
+      name: "Amasya",
+      zoomable: "no"
+    },
+    TUR2291: {
+      url: "./sehir/corum",
+      color: "#4589bc",
+      description: "Kira: 4,432 ₺ <br/> m²: 33₺",
+      name: "Çorum",
+      zoomable: "no"
+    },
+    TUR2292: {
+      url: "./sehir/giresun",
+      color: "#4589bc",
+      description: "Kira: 4,958 ₺ <br/> m²: 42₺",
+      name: "Giresun",
+      zoomable: "no"
+    },
+    TUR2293: {
+      url: "./sehir/ordu",
+      color: "#4589bc",
+      description: "Kira: 5,285 ₺ <br/> m²: 45₺",
+      name: "Ordu",
+      zoomable: "no"
+    },
+    TUR2294: {
+      url: "./sehir/sinop",
+      color: "#4589bc",
+      description: "Kira: 5,041 ₺ <br/> m²: 49₺",
+      name: "Sinop",
+      zoomable: "no"
+    },
+    TUR2295: {
+      url: "./sehir/sivas",
+      color: "#4589bc",
+      description: "Kira: 5,283 ₺ <br/> m²: 42₺",
+      name: "Sivas",
+      zoomable: "no"
+    },
+    TUR2296: {
+      url: "./sehir/samsun",
+      color: "#376e97",
+      description: "Kira: 6,342 ₺ <br/> m²: 60₺",
+      name: "Samsun",
+      zoomable: "no"
+    },
+    TUR2297: {
+      url: "./sehir/tokat",
+      color: "#4589bc",
+      description: "Kira: 4,731 ₺ <br/> m²: 45₺",
+      name: "Tokat",
+      zoomable: "no"
+    },
+    TUR2298: {
+      url: "./sehir/artvin",
+      color: "#90b9d7",
+      description: "Kira: 3,627 ₺ <br/> m²: 28₺",
+      name: "Artvin",
+      zoomable: "no"
+    },
+    TUR2299: {
+      url: "./sehir/erzurum",
+      color: "#90b9d7",
+      description: "Kira: 3,756 ₺ <br/> m²: 27₺",
+      name: "Erzurum",
+      zoomable: "no"
+    },
+    TUR2300: {
+      url: "./sehir/erzincan",
+      color: "#6aa1ca",
+      description: "Kira: 4,029 ₺ <br/> m²: 35₺",
+      name: "Erzincan",
+      zoomable: "no"
+    },
+    TUR2301: {
+      url: "./sehir/rize",
+      color: "#376e97",
+      description: "Kira: 5,333 ₺ <br/> m²: 42₺",
+      name: "Rize",
+      zoomable: "no"
+    },
+    TUR2302: {
+      url: "./sehir/trabzon",
+      color: "#376e97",
+      description: "Kira: 5,873 ₺ <br/> m²: 53₺",
+      name: "Trabzon",
+      zoomable: "no"
+    },
+    TUR2307: {
+      url: "./sehir/agri",
+      color: "#90b9d7",
+      description: "Kira: 2,754 ₺ <br/> m²: 20₺",
+      name: "Ağrı",
+      zoomable: "no"
+    },
+    TUR2308: {
+      url: "./sehir/bingol",
+      color: "#6aa1ca",
+      description: "Kira: 4,236 ₺ <br/> m²: 26₺",
+      name: "Bingöl",
+      zoomable: "no"
+    },
+    TUR2309: {
+      url: "./sehir/diyarbakir",
+      color: "#376e97",
+      description: "Kira: 6,750 ₺ <br/> m²: 45₺",
+      name: "Diyarbakir",
+      zoomable: "no"
+    },
+    TUR2310: {
+      url: "./sehir/mus",
+      color: "#90b9d7",
+      description: "Kira: 2,699 ₺ <br/> m²: 31₺",
+      name: "Muş",
+      zoomable: "no"
+    },
+    TUR2311: {
+      url: "./sehir/bitlis",
+      color: "#90b9d7",
+      description: "Kira: 2,712 ₺ <br/> m²: 26₺",
+      name: "Bitlis",
+      zoomable: "no"
+    },
+    TUR3008: {
+      url: "./sehir/ankara",
+      color: "#295372",
+      description: "Kira: 10,764 ₺ <br/> m²: 88₺",
+      name: "Ankara",
+      zoomable: "no"
+    },
+    TUR3009: {
+      url: "./sehir/cankiri",
+      color: "#4589bc",
+      description: "Kira: 4,583 ₺ <br/> m²: 41₺",
+      name: "Çankırı",
+      zoomable: "no"
+    },
+    TUR3010: {
+      url: "./sehir/zonguldak",
+      color: "#4589bc",
+      description: "Kira: 5,178 ₺ <br/> m²: 52₺",
+      name: "Zonguldak",
+      zoomable: "no"
+    },
+    TUR3011: {
+      url: "./sehir/konya",
+      color: "#295372",
+      description: "Kira: 7,420 ₺ <br/> m²: 53₺",
+      name: "Konya",
+      zoomable: "no"
+    },
+    TUR3012: {
+      url: "./sehir/karaman",
+      color: "#6aa1ca",
+      description: "Kira: 4,150 ₺ <br/> m²: 51₺",
+      name: "Karaman",
+      zoomable: "no"
+    },
+    TUR3014: {
+      url: "./sehir/usak",
+      color: "#6aa1ca",
+      description: "Kira: 4,006 ₺ <br/> m²: 44₺",
+      name: "Uşak",
+      zoomable: "no"
+    },
+    TUR3016: {
+      url: "./sehir/gaziantep",
+      color: "#295372",
+      description: "Kira: 7,897 ₺ <br/> m²: 58₺",
+      name: "Gaziantep",
+      zoomable: "no"
+    },
+    TUR3017: {
+      url: "./sehir/sanliurfa",
+      color: "#4589bc",
+      description: "Kira: 5,135 ₺ <br/> m²: 35₺",
+      name: "Şanlıurfa",
+      zoomable: "no"
+    },
+    TUR3018: {
+      url: "./sehir/adana",
+      color: "#376e97",
+      description: "Kira: 6,448 ₺ <br/> m²: 60₺",
+      name: "Adana",
+      zoomable: "no"
+    },
+    TUR3019: {
+      url: "./sehir/kirikkale",
+      color: "#90b9d7",
+      description: "Kira: 3,307 ₺ <br/> m²: 37₺",
+      name: "Kırıkkale",
+      zoomable: "no"
+    },
+    TUR3020: {
+      url: "./sehir/nigde",
+      color: "#4589bc",
+      description: "Kira: 5,071 ₺ <br/> m²: 36₺",
+      name: "Niğde",
+      zoomable: "no"
+    },
+    TUR3021: {
+      url: "./sehir/aksaray",
+      color: "#4589bc",
+      description: "Kira: 4,895 ₺ <br/> m²: 55₺",
+      name: "Aksaray",
+      zoomable: "no"
+    },
+    TUR3026: {
+      url: "./sehir/yozgat",
+      color: "#6aa1ca",
+      description: "Kira: 4,036 ₺ <br/> m²: 43₺",
+      name: "Yozgat",
+      zoomable: "no"
+    },
+    TUR3031: {
+      url: "./sehir/gumushane",
+      color: "#90b9d7",
+      description: "Kira: 2,652 ₺ <br/> m²: 24₺",
+      name: "Gümüşhane",
+      zoomable: "no"
+    },
+    TUR3032: {
+      url: "./sehir/bayburt",
+      color: "#90b9d7",
+      description: "Kira: 2,941 ₺ <br/> m²: 30₺",
+      name: "Bayburt",
+      zoomable: "no"
+    },
+    TUR3040: {
+      url: "./sehir/kars",
+      color: "#90b9d7",
+      description: "Kira: 3,046 ₺ <br/> m²: 33₺",
+      name: "Kars",
+      zoomable: "no"
+    },
+    TUR3041: {
+      url: "./sehir/mardin",
+      color: "#6aa1ca",
+      description: "Kira: 4,216 ₺ <br/> m²: 29₺",
+      name: "Mardin",
+      zoomable: "no"
+    },
+    TUR3042: {
+      url: "./sehir/batman",
+      color: "#6aa1ca",
+      description: "Kira: 4,099 ₺ <br/> m²: 34₺",
+      name: "Batman",
+      zoomable: "no"
+    },
+    TUR3043: {
+      url: "./sehir/siirt",
+      color: "#90b9d7",
+      description: "Kira: 3,039 ₺ <br/> m²: 24₺",
+      name: "Siirt",
+      zoomable: "no"
+    },
+    TUR3044: {
+      url: "./sehir/sirnak",
+      color: "#90b9d7",
+      description: "Kira: 1,993 ₺ <br/> m²: 27₺",
+      name: "Şırnak",
+      zoomable: "no"
+    },
+    TUR3045: {
+      url: "./sehir/tunceli",
+      color: "#376e97",
+      description: "Kira: 5,323 ₺ <br/> m²: 34₺",
+      name: "Tunceli",
+      zoomable: "no"
+    },
+    TUR3047: {
+      url: "./sehir/hakkari",
+      color: "#90b9d7",
+      description: "Kira: 1,800 ₺ <br/> m²: 29₺",
+      name: "Hakkari",
+      zoomable: "no"
+    },
+    TUR3048: {
+      url: "./sehir/van",
+      color: "#4589bc",
+      description: "Kira: 4,499 ₺ <br/> m²: 44₺",
+      name: "Van",
+      zoomable: "no"
+    },
+    TUR4839: {
+      url: "./sehir/ardahan",
+      color: "#90b9d7",
+      description: "Kira: 3,574 ₺ <br/> m²: 42₺",
+      name: "Ardahan",
+      zoomable: "no"
+    },
+    TUR4840: {
+      url: "./sehir/igdir",
+      color: "#90b9d7",
+      description: "Kira: 3,737 ₺ <br/> m²: 31₺",
+      name: "Iğdır",
+      zoomable: "no"
+    },
+    TUR4841: {
+      url: "./sehir/kilis",
+      color: "#6aa1ca",
+      description: "Kira: 4,063 ₺ <br/> m²: 37₺",
+      name: "Kilis",
+      zoomable: "no"
+    },
+    TUR4842: {
+      url: "./sehir/osmaniye",
+      color: "#6aa1ca",
+      description: "Kira: 4,322 ₺ <br/> m²: 35₺",
+      name: "Osmaniye",
+      zoomable: "no"
+    },
+    TUR5518: {
+      url: "./sehir/yalova",
+      color: "#295372",
+      description: "Kira: 9,089 ₺ <br/> m²: 72₺",
+      name: "Yalova",
+      zoomable: "no"
+    },
+    TUR5519: {
+      url: "./sehir/duzce",
+      color: "#6aa1ca",
+      description: "Kira: 4,290 ₺ <br/> m²: 45₺",
+      name: "Düzce",
+      zoomable: "no"
+    },
+    TUR5520: {
+      url: "./sehir/karabuk",
+      color: "#6aa1ca",
+      description: "Kira: 4,219 ₺ <br/> m²: 48₺",
+      name: "Karabük",
+      zoomable: "no"
+    },
+    TUR5521: {
+      url: "./sehir/bartin",
+      color: "#376e97",
+      description: "Kira: 5,364 ₺ <br/> m²: 59₺",
+      name: "Bartın",
+      zoomable: "no"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {},
+  data: {
+    data: {
+      TUR2229: "9814",
+      TUR2230: "11971",
+      TUR2238: "7720",
+      TUR2239: "7132",
+      TUR2240: "6411",
+      TUR2241: "5336",
+      TUR2242: "7217",
+      TUR2263: "4236",
+      TUR2264: "8405",
+      TUR2265: "15014",
+      TUR2266: "8761",
+      TUR2267: "7352",
+      TUR2268: "5012",
+      TUR2269: "5592",
+      TUR2270: "3924",
+      TUR2271: "13747",
+      TUR2272: "4000",
+      TUR2273: "3774",
+      TUR2274: "5474",
+      TUR2275: "4399",
+      TUR2276: "3662",
+      TUR2277: "6336",
+      TUR2278: "27516",
+      TUR2281: "5722",
+      TUR2282: "4350",
+      TUR2283: "5861",
+      TUR2284: "6571",
+      TUR2285: "12150",
+      TUR2286: "4184",
+      TUR2287: "5103",
+      TUR2288: "6094",
+      TUR2289: "8493",
+      TUR2290: "4353",
+      TUR2291: "4432",
+      TUR2292: "4958",
+      TUR2293: "5285",
+      TUR2294: "5041",
+      TUR2295: "5283",
+      TUR2296: "6342",
+      TUR2297: "4731",
+      TUR2298: "3627",
+      TUR2299: "3756",
+      TUR2300: "4029",
+      TUR2301: "5333",
+      TUR2302: "5873",
+      TUR2307: "2754",
+      TUR2308: "4236",
+      TUR2309: "6750",
+      TUR2310: "2699",
+      TUR2311: "2712",
+      TUR3008: "10764",
+      TUR3009: "4583",
+      TUR3010: "5178",
+      TUR3011: "7420",
+      TUR3012: "4150",
+      TUR3014: "4006",
+      TUR3016: "7897",
+      TUR3017: "5135",
+      TUR3018: "6448",
+      TUR3019: "3307",
+      TUR3020: "5071",
+      TUR3021: "4895",
+      TUR3026: "4036",
+      TUR3031: "2652",
+      TUR3032: "2941",
+      TUR3040: "3046",
+      TUR3041: "4216",
+      TUR3042: "4099",
+      TUR3043: "3039",
+      TUR3044: "1993",
+      TUR3045: "5323",
+      TUR3047: "1800",
+      TUR3048: "4499",
+      TUR4839: "3574",
+      TUR4840: "3737",
+      TUR4841: "4063",
+      TUR4842: "4322",
+      TUR5518: "9089",
+      TUR5519: "4290",
+      TUR5520: "4219",
+      TUR5521: "5364"
+    }
+  }
+};
